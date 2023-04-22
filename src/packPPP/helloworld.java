bbcd82f13cf5325bfe33c43acaae588fbae00660
rameshwarhr <106536097+rameshwarhr@users.noreply.github.com>
2023-04-22 13:55:44 +0530

    add addition program in helloworld class

--- src/packPPP/helloworld.java
+++ src/packPPP/helloworld.java
@@ -2,8 +2,16 @@ package packPPP;
 
 public class helloworld 
 {
-	public static void main(String []args) {
+	public static void main(String []args) 
+	{
 		System.out.println("hello");
+		
+		int a=10;
+		int b=25;
+		System.out.println(a+b);
+		
 	}
+	
+	
 
 }
